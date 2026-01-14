014bbf4fb32d6e7ce10f0a89fe476d571e1f050d
nkunda-prince <50352619+nkunda-prince@users.noreply.github.com>
2026-01-14 13:45:44 +0200

    Update App.jsx

--- alx-react-app/src/components/App.jsx
+++ alx-react-app/src/components/App.jsx
@@ -1,6 +1,7 @@
 import Header from './Header';
 import MainContent from './MainContent';
 import Footer from './Footer';
+import UserProfile from './components/UserProfile';
 
 function App() {
     return (
