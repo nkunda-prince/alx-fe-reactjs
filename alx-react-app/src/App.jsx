0e4b54027c13dcd6189644ef068fcdc03b2f7112
nkunda-prince <50352619+nkunda-prince@users.noreply.github.com>
2026-01-14 13:51:49 +0200

    Update App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,6 +2,7 @@ import UserProfile from './components/UserProfile';
 import Header from './Header';
 import MainContent from './MainContent';
 import Footer from './Footer';
+import WelcomeMessage from './components/WelcomeMessage';
 
 function App() {
     return (
