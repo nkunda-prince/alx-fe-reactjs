78267456104c16e17eec0cb8402c740f5d5fae58
nkunda-prince <50352619+nkunda-prince@users.noreply.github.com>
2026-01-21 16:32:52 +0200

    Create MainContent.jsx

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,19 @@
+const MainContent = () => {
+  return (
+    <main
+      style={{
+        padding: '20px',
+        backgroundColor: '#eef2f7',
+        minHeight: '300px'
+      }}
+    >
+      <h2 style={{ color: '#333' }}>Welcome to My Travel Page</h2>
+      <p style={{ lineHeight: '1.6' }}>
+        Here you will find information about some of my favorite cities around
+        the world.
+      </p>
+    </main>
+  );
+};
+
+export default MainContent;
