12dd10a50c719d400d26192ae842e3e586ff1a15
nkunda-prince <50352619+nkunda-prince@users.noreply.github.com>
2026-01-21 16:33:17 +0200

    Create Footer.jsx

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,17 @@
+const Footer = () => {
+  return (
+    <footer
+      style={{
+        backgroundColor: '#222',
+        color: 'white',
+        textAlign: 'center',
+        padding: '10px',
+        marginTop: '20px'
+      }}
+    >
+      <p>&copy; 2026 My React App. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
