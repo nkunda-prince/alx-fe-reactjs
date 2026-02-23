97aa9f7b4bdca4ea1ef8e49fa4e59f17eabe8b63
nkunda-prince <50352619+nkunda-prince@users.noreply.github.com>
2026-02-23 12:11:41 +0200

    Update RecipeDetails.jsx

--- recipe-sharing-app/src/components/RecipeDetails.jsx
+++ recipe-sharing-app/src/components/RecipeDetails.jsx
@@ -1,1 +1,29 @@
+import { useParams, Link } from "react-router-dom";
+import { useRecipeStore } from "./recipeStore";
+import EditRecipeForm from "./EditRecipeForm";
+import DeleteRecipeButton from "./DeleteRecipeButton";
 
+const RecipeDetails = () => {
+  const { id } = useParams();
+  const recipeId = Number(id);
+
+  const recipe = useRecipeStore((state) =>
+    state.recipes.find((recipe) => recipe.id === recipeId)
+  );
+
+  if (!recipe) return <p>Recipe not found</p>;
+
+  return (
+    <div>
+      <Link to="/">← Back</Link>
+
+      <h2>{recipe.title}</h2>
+      <p>{recipe.description}</p>
+
+      <EditRecipeForm recipe={recipe} />
+      <DeleteRecipeButton id={recipe.id} />
+    </div>
+  );
+};
+
+export default RecipeDetails;
