e8306962829f419ed0f0ff643532913846aade70
nkunda-prince <50352619+nkunda-prince@users.noreply.github.com>
2026-02-23 12:36:33 +0200

    Update RecipeDetails.jsx

--- recipe-sharing-app/src/components/RecipeDetails.jsx
+++ recipe-sharing-app/src/components/RecipeDetails.jsx
@@ -9,6 +9,11 @@ const RecipeDetails = () => {
 
   const recipe = useRecipeStore((state) =>
     state.recipes.find((recipe) => recipe.id === recipeId)
+const favorites = useRecipeStore((state) => state.favorites);
+const addFavorite = useRecipeStore((state) => state.addFavorite);
+const removeFavorite = useRecipeStore((state) => state.removeFavorite);
+
+const isFavorite = favorites.includes(recipe.id);
   );
 
   if (!recipe) return <p>Recipe not found</p>;
