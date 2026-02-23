d3dcac1809c7c67b393da66346755b257fe9ae64
nkunda-prince <50352619+nkunda-prince@users.noreply.github.com>
2026-02-23 12:39:24 +0200

    Update FavoritesList.jsx

--- recipe-sharing-app/src/components/FavoritesList.jsx
+++ recipe-sharing-app/src/components/FavoritesList.jsx
@@ -1,1 +1,27 @@
+import { useRecipeStore } from "./recipeStore";
 
+const FavoritesList = () => {
+  const recipes = useRecipeStore((state) => state.recipes);
+  const favorites = useRecipeStore((state) => state.favorites);
+
+  const favoriteRecipes = favorites
+    .map((id) => recipes.find((recipe) => recipe.id === id))
+    .filter(Boolean);
+
+  return (
+    <div style={{ marginTop: "30px" }}>
+      <h2>My Favorites</h2>
+
+      {favoriteRecipes.length === 0 && <p>No favorites yet.</p>}
+
+      {favoriteRecipes.map((recipe) => (
+        <div key={recipe.id}>
+          <h4>{recipe.title}</h4>
+          <p>{recipe.description}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default FavoritesList;
